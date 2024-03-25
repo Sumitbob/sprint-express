bf2224309371c04410f14c941337c5cc9d8782f3
paramanand-balara <paramanand.balara@credilio.in>
2024-03-25 22:51:06 +0530

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const {ErrorHandler} = require('./Middlewares/Handlers');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+const Routes = require('./Routes');
+
+router.get('/', (req, res) => {
+	res.json({success: true});
+});
+
+app.use(router);
+app.use(Routes);
+app.use(ErrorHandler.handle);
+module.exports = app;
