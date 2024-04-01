15b3b12f2ab1e9c46a0025ed67f02be2bf2f4b33
paramanand-balara <paramanand.balara@credilio.in>
2024-04-01 15:52:24 +0530

    cgi-example-deleted

--- example_cgi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Content-type: text/html"
-echo ""
-echo "<html><body>"
-echo "<h1>Hello, CGI!</h1>"
-echo "<p>This is a simple CGI script running on macOS.</p>"
-echo "</body></html>"
