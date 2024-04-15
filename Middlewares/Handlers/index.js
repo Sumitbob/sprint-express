255c33b7e7d7f532aa4c8c458aefb65bb0cf9bf8
paramanand-balara <paramanand.balara@credilio.in>
2024-04-15 23:19:44 +0530

    file-removed

--- Middlewares/Handlers/index.js
+++ Middlewares/Handlers/index.js
@@ -2,12 +2,10 @@ const ErrorHandler = require('./ErrorHandler');
 const NotFoundError = require('./NotFoundError');
 const UnauthorizedError = require('./UnauthorizedError');
 const ValidationError = require('./ValidationError');
-const DatabaseErrorHandler = require('./DatabaseErrorHandler');
 
 module.exports = {
 	ErrorHandler,
 	NotFoundError,
 	UnauthorizedError,
 	ValidationError,
-	DatabaseErrorHandler,
 };
