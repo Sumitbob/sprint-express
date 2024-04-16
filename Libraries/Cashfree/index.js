baf62b0d570f3f8a89630e9576f92123687b826f
paramanand-balara <paramanand.balara@credilio.in>
2024-04-16 23:00:39 +0530

    unwanted code removed

--- Libraries/Cashfree/index.js
+++ Libraries/Cashfree/index.js
@@ -32,16 +32,4 @@ class Cashfree {
 
 	}
 }
-const data = {
-	customerDetails: {
-		customerId: '7112AAA812236',
-		customerEmail: 'paramanand982@gmail.com',
-		customerPhone: '7239887406',
-		customerName: 'Paramanand'
-	},
-	orderId: 'test-ord-11',
-	orderAmount: 10,
-	orderCurrency: 'INR'
-};
-new Cashfree().createPaymentOrder(data);
-// module.exports = new Cashfree();
+module.exports = new Cashfree();
